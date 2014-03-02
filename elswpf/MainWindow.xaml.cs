0a626235f26612b17cd4523e3cfa77e62ebaff6b
kodack64 <kodack64@msn.com>
2014-03-02 12:25:47 +0900

    色�

--- elswpf/MainWindow.xaml.cs
+++ elswpf/MainWindow.xaml.cs
@@ -12,14 +12,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace elswpf {
 	/// <summary>
 	/// MainWindow.xaml の相互作用ロジック
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow : Elysium.Controls.Window {
 		public MainWindow() {
 			InitializeComponent();
+			this.MouseLeftButtonDown += (sender, e) => this.DragMove();
+		}
+		private void TabControl1_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+		}
+		private static readonly string Windows = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+		private static readonly string SegoeUI = Windows + @"\Fonts\SegoeUI.ttf";
+		private static readonly string Verdana = Windows + @"\Fonts\Verdana.ttf";
+		private static readonly string MSGothic = Windows + @"\Fonts\msgothic.ttc";
+		private void AccentGlyphInitialized(object sender, EventArgs e) {
+			AccentGlyph.FontUri = new Uri(File.Exists(SegoeUI) ? SegoeUI : Verdana);
+		}
+		private void ChamberGlyphInitialized(object sender, EventArgs e) {
+			ChamberGlyph.FontUri = new Uri(MSGothic);
 		}
 	}
 }
